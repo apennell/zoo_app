05430fb7cdb22d010a86c7995f20ab6925fc3408
Annie Pennell <annie@anniepennell.com>
2015-08-02 16:15:28 -0700

    added nav bar and nav buttons; changed to form_for

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,2 +1,4 @@
 class Animal < ActiveRecord::Base
+  # has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
+  # validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end

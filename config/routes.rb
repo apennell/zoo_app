955a3d83d1b3a7ffa110826728c0d6d9468f0443
Annie Pennell <annie@anniepennell.com>
2015-08-05 15:01:53 -0700

    new user works

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,28 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/show'
+
   get 'exhibits/desert'
 
   get 'exhibits/forest'
 
   get 'exhibits/town'
 
+  get '/login', to: 'sessions#new'
+
+  post '/login', to: 'sessions#create'
+
+  get '/logout', to: 'sessions#destroy'
+
   root to: 'animals#index'
 
-  resources :animals, :exhibits
+  resources :animals, :exhibits, :users
 
   # delete '/animals/:id', to: 'animals#destroy'
 
